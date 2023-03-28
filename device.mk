414ecb01dc4dd4a9e510ace0b6dad35a83cf8947
AmeChanRain <downloadbot007@gmail.com>
2023-03-28 20:02:43 +0000

    odin: Stop building LiveDisplay

--- device.mk
+++ device.mk
@@ -250,10 +250,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.lights-service.qti
 
-# LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service-sdm
-
 # Media
 PRODUCT_PACKAGES += \
     libavservices_minijail \
