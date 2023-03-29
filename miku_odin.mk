45c45cf3c45b1ebd58d2b86b215b87c15805bd16
keiko233 <keiko23333@gmail.com>
2023-03-29 14:35:52 +0000

    odin: Build MIKU_GAPPS by default

--- miku_odin.mk
+++ miku_odin.mk
@@ -23,5 +23,8 @@ TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := odin
 PRODUCT_BUILD_PROP_OVERRIDES += PRIVATE_BUILD_DESC="missi-user 13 TKQ1.220829.002 V14.0.1.0.TKMCNXM release-keys"
 
+# Build MIKU_GAPPS
+MIKU_GAPPS := true
+
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := Xiaomi/odin/odin:13/TKQ1.220829.002/V14.0.1.0.TKMCNXM:user/release-keys
