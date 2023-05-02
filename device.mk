188ab4ee1587a76e7f0df10582bac71d83f10290
KujouYuko <i@779.moe>
2023-05-02 11:54:09 +0000

    odin: Setup dalvik vm configs
    * Fix the problem of partial application OOM after removing libinit.

--- device.mk
+++ device.mk
@@ -1,6 +1,9 @@
 # Enable Scoped Storage related
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 
+# Setup dalvik vm configs
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # A/B
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota.mk)
 
